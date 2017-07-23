4177461c1ad8e461e8216799f376418474cb7084
Paul <paucho1437@yahoo.com>
2017-07-23 14:40:28 -0500

    data fetched and displayed

--- project.pl
+++ project.pl
@@ -14,37 +14,19 @@ or die $DBI::errstr;
 print "Opened database successfully\n";
 
 
-my $stmt = qq(INSERT INTO PERSON (ID,FIRSTNAME,LASTNAME,HOME)
-VALUES (1, 'Rose', 'Tyler', 'Earth' ));
-my $rv = $dbh->do($stmt) or die $DBI::errstr;
+my $stmt = qq(SELECT id, firstname, lastname, home from PERSON;);
+my $sth = $dbh->prepare( $stmt );
+my $rv = $sth->execute() or die $DBI::errstr;
 
-$stmt = qq(INSERT INTO PERSON (ID,FIRSTNAME,LASTNAME,HOME)
-VALUES (2, 'Zoe', 'Hariot', 'Space Station W3' ));
-$rv = $dbh->do($stmt) or die $DBI::errstr;
+if($rv < 0) {
+    print $DBI::errstr;
+}
 
-$stmt = qq(INSERT INTO PERSON (ID,FIRSTNAME,LASTNAME,HOME)
-VALUES (3, 'Jo', 'Grant', 'Earth' ));
-$rv = $dbh->do($stmt) or die $DBI::errstr;
-
-$stmt = qq(INSERT INTO PERSON (ID,FIRSTNAME,LASTNAME,HOME)
-VALUES (4, 'Leela', null, 'Unspecified' ));
-$rv = $dbh->do($stmt) or die $DBI::errstr;
-
-$stmt = qq(INSERT INTO PERSON (ID,FIRSTNAME,LASTNAME,HOME)
-VALUES (5, 'Romana', null, 'Gallifrey' ));
-$rv = $dbh->do($stmt) or die $DBI::errstr;
-
-$stmt = qq(INSERT INTO PERSON (ID,FIRSTNAME,LASTNAME,HOME)
-VALUES (6, 'Clara', 'Oswald', 'Earth' ));
-$rv = $dbh->do($stmt) or die $DBI::errstr;
-
-$stmt = qq(INSERT INTO PERSON (ID,FIRSTNAME,LASTNAME,HOME)
-VALUES (7, 'Adric', null, 'Alzarius' ));
-$rv = $dbh->do($stmt) or die $DBI::errstr;
-
-$stmt = qq(INSERT INTO PERSON (ID,FIRSTNAME,LASTNAME,HOME)
-VALUES (8, 'Susan', 'Foreman', 'Gallifrey' ));
-$rv = $dbh->do($stmt) or die $DBI::errstr;
-
-print "Records created successfully\n";
+while(my @row = $sth->fetchrow_array()) {
+    print "ID = ". $row[0] . "\n";
+    print "FIRSTNAME = ". $row[1] ."\n";
+    print "LASTNAME = ". $row[2] ."\n";
+    print "HOME =  ". $row[3] ."\n\n";
+}
+print "Operation done successfully\n";
 $dbh->disconnect();
