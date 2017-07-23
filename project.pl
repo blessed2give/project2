50b2bbe81ec0c3de4fb35f348a5538018524eb08
Paul <paucho1437@yahoo.com>
2017-07-23 14:16:12 -0500

    table created

--- project.pl
+++ project.pl
@@ -12,3 +12,17 @@ my $dbh = DBI->connect($dsn, $userid, $password, { RaiseError => 1 })
 or die $DBI::errstr;
 
 print "Opened database successfully\n";
+
+my $stmt = qq(CREATE TABLE PERSON
+(ID INT PRIMARY KEY     NOT NULL,
+FIRSTNAME           TEXT,
+LASTNAME            TEXT,
+HOME         TEXT););
+
+my $rv = $dbh->do($stmt);
+if($rv < 0) {
+    print $DBI::errstr;
+} else {
+    print "Table created successfully\n";
+}
+$dbh->disconnect();
